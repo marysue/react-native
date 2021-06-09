c78174f32df6baeb6c6b5685e8a0e1e4f25e8025
marysue <mary@marylark.com>
2021-06-08 18:18:08 -0600

    Deleting child item, binding item.id to function call

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -11,12 +11,21 @@ export default function App() {
     console.log(goalTitle);
     //setCourseGoals([...courseGoals, enteredGoal]);
     //To avoid latency issues, use anonymous function to spread courseGoals ...
-    setCourseGoals(currentGoals => [...currentGoals, { id: Math.random().toString(), value: goalTitle}]);
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      { id: Math.random().toString(), value: goalTitle}
+    ]);
+  }
+
+  const removeGoalHandler = (goalId) => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.id !== goalId)
+    });
   }
 
   return (
     <View style={styles.screen}>
-      <GoalInput onAddGoal={addGoalHandler}></GoalInput>
+      <GoalInput onDelete={removeGoalHandler} onAddGoal={addGoalHandler}></GoalInput>
 
     {/* <ScrollView>
         {courseGoals.map((goal, idx) => {return (
@@ -34,9 +43,11 @@ export default function App() {
           keyExtractor={(item, index) => item.id}
           data={courseGoals}
           renderItem={ itemData => (
-            <GoalItem title={itemData.item.value} />
+            <GoalItem title={itemData.item.value} id={itemData.item.id} onDelete={removeGoalHandler}/>
           )
         }
+        // Or you can just bind the removeGoalHandler to the id when setting your props as follows:
+        // onDelete={removeGoalHandler.bind(this, itemData.item.id)}
         />
 
     </View>
