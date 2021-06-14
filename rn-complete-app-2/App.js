1c21fb2ddfc4afc989151d30de792b389574a2eb
marysue <mary@marylark.com>
2021-06-14 16:30:37 -0600

    Created header component with styling

---
+++ rn-complete-app-2/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header';
+
+export default function App() {
+  return (
+    <View style={styles.screen}>
+      <Header title="Guess a Number" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  // flex: 1 ensures that it takes up all the space it can get
+  screen: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
